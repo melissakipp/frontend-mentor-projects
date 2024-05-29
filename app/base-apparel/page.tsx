d02e483a5e61eadb3d80e2c8306d1b06ee4d9cd3
Melissa Kipp <melissakipp.az@gmail.com>
2024-05-28 18:52:19 -0700

    Fixes

--- app/base-apparel/page.tsx
+++ app/base-apparel/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Image from 'next/image'
+import Image from 'next/image';
 import { useState, useLayoutEffect } from 'react';
 
 import './base.css';
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,27 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import Image from "next/image";
+import cardImage from "../public/base-apparel-images/Screenshot-2024-05-27_first-draft.png";
 
 export default function Home() {
   return (
     <main className={styles.homePage}>
       <article>
-      <h1>Frontend Mentor</h1>
+      <h1 className={styles.pageTitle}>Frontend Mentor</h1>
       <section className={styles.challenges}>
-        <h2>Newbie</h2>
-        <ul>
+        <h2 className={styles.title} >Newbie</h2>
+        <ul className={styles.cardList}>
           <li className={styles.card}>
             <Link href="/base-apparel">
+              <h3 className={styles.cardTitle}>
+                Base Apparel Coming Soon Page
+              </h3>
               <Image
-              src="/base-apparel-images/Screenshot-2024-05-27_first-draft.png"
+              className={styles.cardImg}
+              src={cardImage}
               alt=""
               aria-hidden="true"
-              width={300}
-              height={300}
               />
-                Base Apparel Coming Soon Page
             </Link>
           </li>
         </ul>
