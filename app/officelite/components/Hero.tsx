1811a145b2a51ed62ef485a38d7459f29d46922a
melissakipp <melissakipp.az@gmail.com>
2025-06-10 20:15:18 -0700

    Add content

--- app/officelite/components/Hero.tsx
+++ app/officelite/components/Hero.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 import styles from './Hero.module.css';
+import HeroImage from '@/public/images/officelite/home/illustration-charts.svg';
 
 export default function Hero() {
   return (
-    <div className={styles.container}>Hero</div>
-  )
+    <section className={styles.container}>
+      <div className={styles.colLeft}>
+        <Image src={HeroImage} alt="Officelite Hero Image" priority />
+      </div>
+      <h2 className={styles.title}>A simple solution to complex tasks is coming soon</h2>
+      <p className={styles.description}>
+        Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new collaboration platform built with an intuitive interface to improve productivity.
+      </p>
+      <Link href="/officelite" className={styles.button}>
+        Get Started
+      </Link>
+    </section>
+  );
 }
