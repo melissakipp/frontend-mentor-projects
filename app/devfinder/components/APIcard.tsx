34782fb488028f366c494847be7f12e12e88fe72
melissakipp <melissakipp.az@gmail.com>
2025-07-01 09:10:00 -0700

    Test files and imports

---
+++ app/devfinder/components/APIcard.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/APIcard.module.css';
+
+export default function APIcard() {
+  return (
+    <div className={styles.card}>APIcard</div>
+  );
+}
---
+++ app/devfinder/components/SearchBar.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/SeachBar.module.css';
+
+export default function SearchBar() {
+  return (
+    <div className={styles.container}>SearchBar</div>
+  )
+}
