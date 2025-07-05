5f0a597f31576247aeb371ea0c02a00aac1586ee
melissakipp <melissakipp.az@gmail.com>
2025-07-05 11:37:56 -0700

    Update: add GitHub avatar URLs for API image calls

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,12 @@ const nextConfig = {
   images: {
     domains: [],
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   },
   webpack: (config, { isServer }) => {
     config.watchOptions = {
