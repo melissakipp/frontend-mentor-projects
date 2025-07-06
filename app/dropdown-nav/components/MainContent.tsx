37d4f1eccbcebcd0e6b34ae55a43ed0ec8f9da69
melissakipp <melissakipp.az@gmail.com>
2025-07-06 07:46:29 -0700

    Code clean up

--- app/dropdown-nav/components/MainContent.tsx
+++ app/dropdown-nav/components/MainContent.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import styles from "./MainContent.module.css";
-import Testmonials from "./Testmonials";
+import styles from './MainContent.module.css';
+import Testmonials from './Testmonials';
 import HeroMobileImg from '../../../public/images/dropdown-nav/image-hero-mobile.png';
 import HeroDesktopImg from '../../../public/images/dropdown-nav/image-hero-desktop.png';
 
@@ -12,13 +12,13 @@ export default function MainContent() {
       <section className={styles.hero}>
         <picture>
           <source
-            media="(min-width: 1024px)"
+            media='(min-width: 1024px)'
             srcSet={HeroDesktopImg.src}
           />
           <Image
             className={styles.heroImg}
             src={HeroMobileImg}
-            alt="Illustration"
+            alt='Illustration'
             width={500}
             height={500}
             priority
@@ -26,14 +26,14 @@ export default function MainContent() {
         </picture>   
       </section>
       <section className={`${styles.pageText} ${styles.leftCol}`}>
-        <h1 className="sr-only">Snap Home page</h1>
+        <h1 className='sr-only'>Snap Home page</h1>
         <h2 className={styles.pageText__title}>Make remote work</h2>
         <p className={styles.pageText__description}>
           Get your team in sync, no matter your location. Streamline processes,
           create team rituals, and watch productivity soar.
         </p>
         <div>
-          <a className={styles.btn} href="">Learn more</a>
+          <a className={styles.btn} href=''>Learn more</a>
         </div>
       </section>
       <Testmonials />
